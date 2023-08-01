0104b3e8b46b2e5968af3450a25903bb364648af
Anastasiya <ronas0709@gmail.com>
2023-08-01 11:21:51 +0200

    solve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarwarsService } from './starwars.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Star Wars roster</h1>
+    <ul>
+      <li *ngFor="let person of people">{{ person.name }}</li>
+    </ul>
+  `,
 })
-export class AppComponent {
-  title = 'api';
+export class AppComponent implements OnInit {
+  people: any[] = [];
+
+  constructor(private readonly starwarsService: StarwarsService) {}
+
+  ngOnInit() {
+    this.starwarsService.getPeople().subscribe((data) => {
+      this.people = data.results;
+    });
+  }
 }
--- src/app/starwars.service.ts
+++ src/app/starwars.service.ts
@@ -1,9 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StarwarsService {
+  private readonly apiUrl = 'https://swapi.dev/api/people/';
+
   constructor(private readonly http: HttpClient) {}
+
+  getPeople(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 }
